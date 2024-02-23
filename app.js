63f6a89f905f5451bf819a71b909e86b8ba53b44
Ade830 <adewole.travittlee@gmail.com>
2024-02-23 08:05:42 -0500

    adding code from 2.22.24 class to repo

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+//
+const changeButton = () => {
+    
+    //this is where I put my instructions
+    
+    let ourButton = document.querySelector('.attnBtn')
+    ourButton.innerHTML = "Completed"
+
+})
